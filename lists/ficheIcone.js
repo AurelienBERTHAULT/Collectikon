e1d3e7454a848cbd80c7334a5e1247f5855bb570
Myozotyss <aurelien.berthault@utt.fr>
2013-01-13 14:18:24 +0100

    essai

---
+++ lists/ficheIcone.js
@@ -0,0 +1,30 @@
+function(head, req) {
+
+  // !json templates.ficheIcone
+  // !code lib/mustache.js
+
+  start({"headers":{"Content-Type":"text/html;charset=utf-8"}});
+
+  var data = {
+    icone: []
+  };
+
+  // On récupère l'objet JSON de la requête
+  while (row = getRow()) {
+    	data.icone.push({
+
+        	id: row.value._id,
+                couleur: row.value.couleur,
+                date: row.value.date,
+                forme: row.value.forme,
+                nom: row.value.nom,
+                representation: row.value.representation,
+                statut: row.value.statut,
+                sujet: row.value.sujet,
+                type: row.value.type
+    	});
+  }
+
+  //On applique les valeurs à la page html comments
+  return Mustache.to_html(templates.ficheIcone, data);
+}
